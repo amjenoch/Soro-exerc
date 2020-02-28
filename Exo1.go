26ca4618b0076899812c6acd4754bd1b4557cbfb
amjenoch <58947246+amjenoch@users.noreply.github.com>
2020-02-28 16:07:44 +0000

    Update Exo1.go

--- Exo1.go
+++ Exo1.go
@@ -1,25 +1,19 @@
 package main
 
-
-
 import "fmt"
 
-
 func main() {
-	T:= 0
+	T := 0
+
 	fmt.Print("Entrez la température:")
 	fmt.Scan(&T)
-	
-	if T <= 0 {
-		fmt.Println("c'est de la glace")
-	}
-	if 0<T && T<100{
-		fmt.Println("C'est liquide")
-	}
 
-	if T >=100 {
+	if T <= 0{
+		fmt.Println("c'est de la glace")
+	} else if T < 100{
+		fmt.Println("C'est liquide")
+	} else {
 		fmt.Println("c'est de la vapeur")
 	}
-
 
 }
